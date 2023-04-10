9125deb36afc849420042fb526d42f8dc0284a99
Alex Groeger <groeger.2@wright.edu>
2023-04-10 15:22:10 -0500

    added timeit for tensorflow

---
+++ tensorflow/timeit.py
@@ -0,0 +1,24 @@
+import numpy as np
+from time import time
+
+def timeit(f, runs=20, timeout=120):
+    
+    times = []
+    total_time = 0
+
+    for r in range(runs):
+
+        if total_time >= timeout:
+            break
+        
+        s = time()
+        
+        f()
+
+        dt = time() - s
+        times.append(dt)
+        total_time += dt
+
+    median_time = np.sort(times)[len(times)//2]
+
+    return median_time
