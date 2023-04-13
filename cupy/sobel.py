9d8e408e8cafd86d09c9d045e632ec3b4ee30a27
Alex Groeger <groeger.2@wright.edu>
2023-04-12 21:49:13 -0500

    tried using same library for cpu and gpu

--- cupy/sobel.py
+++ cupy/sobel.py
@@ -3,8 +3,7 @@ os.environ["CUPY_ACCELERATORS"] = 'cub'
 
 import numpy as np
 import cupy as cp
-from scipy.ndimage inport sobel as cpuSobel
-from cupyx.scipy.ndimage import gpuSobel
+from cupyx.scipy.ndimage import sobel
 from timeit import timeit
 
 n=1024
@@ -13,12 +12,12 @@ print(f"Size: {n}x{n}")
 a = np.random.randint(0,255,size=(n,n),dtype=np.uint8)
 ga = cp.array(a)
 
-ta = timeit("small_opencv",lambda: cpuSobel(a),timeout=20)
+ta = timeit("small_opencv",lambda: sobel(a),timeout=20)
 print(f"cpu time: {ta:.4e}")
-tb = timeit("small_cupy",lambda: gpuSobel(ga),timeout=10,gsync=True)
+tb = timeit("small_cupy",lambda: sobel(ga),timeout=10,gsync=True)
 print(f"gpu time: {tb:.4e}")
 print(f"operation speedup: {ta/tb:.2f}")
-tc = timeit("small_cupy_memory",lambda: gpuSobel(cp.array(a)).get(),timeout=10,gsync=True)
+tc = timeit("small_cupy_memory",lambda: sobel(cp.array(a)).get(),timeout=10,gsync=True)
 print(f"gpu+mem time: {tc:.4e}")
 print(f"total speedup: {ta/tc:.2f}")
 
@@ -32,11 +31,11 @@ print(f"Size: {N}x{N}")
 A = np.random.randint(0,255,size=(N,N),dtype=np.uint8)
 gA = cp.array(A)
 
-tA = timeit("big_opencv",lambda: cpuSobel(A),timeout=60)
+tA = timeit("big_opencv",lambda: sobel(A),timeout=60)
 print(f"cpu time: {tA:.4e}")
-tB = timeit("big_cupy",lambda: gpuSobel(gA),timeout=30,gsync=True)
+tB = timeit("big_cupy",lambda: sobel(gA),timeout=30,gsync=True)
 print(f"gpu time: {tB:.4e}")
 print(f"operation speedup: {tA/tB:.2f}")
-tC = timeit("big_cupy_memory",lambda: gpuSobel(cp.array(gA)).get(),timeout=30,gsync=True)
+tC = timeit("big_cupy_memory",lambda: sobel(cp.array(gA)).get(),timeout=30,gsync=True)
 print(f"gpu+mem time: {tC:.4e}")
 print(f"total speedup: {tA/tC:.2f}")
