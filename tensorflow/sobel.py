de499ccca122d87efc8dd51b49c753c5c7762e3a
LakshmiBommareddy3005 <128531809+LakshmiBommareddy3005@users.noreply.github.com>
2023-04-13 11:56:07 -0400

    Update sobel.py

--- tensorflow/sobel.py
+++ tensorflow/sobel.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import time
 
 # Generate a random 512x512 matrix
-input_data = tf.random.normal([2048,2048])
+input_data = tf.random.normal([512,512])
 
 # Create the Sobel filter
 sobel_filter = tf.constant([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=tf.float32)
@@ -12,13 +12,13 @@ sobel_filter = tf.reshape(sobel_filter, [3, 3, 1, 1])
 # Create a function for applying the filter to the input matrix
 def apply_sobel_filter(input_data):
     # Reshape the input matrix into a 4D tensor (batch_size=1, height=512, width=512, channels=1)
-    input_data = tf.reshape(input_data, [1, 2048,2048, 1])
+    input_data = tf.reshape(input_data, [1, 512,512, 1])
 
     # Apply the Sobel filter to the input matrix using conv2d
     output_data = tf.nn.conv2d(input_data, sobel_filter, strides=[1, 1, 1, 1], padding='SAME')
 
     # Reshape the output matrix back to a 2D matrix (height=512, width=512)
-    output_data = tf.reshape(output_data,2048,2048])
+    output_data = tf.reshape(output_data,512,512])
 
     return output_data
 
