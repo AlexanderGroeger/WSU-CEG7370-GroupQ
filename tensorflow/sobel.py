4e9a5e6cf48e910418e09dc545cde76e701abb10
Lakshmi Bommareddy <w306lxb@login01.cluster>
2023-04-18 21:27:22 -0500

    update Resize on image

--- tensorflow/sobel.py
+++ tensorflow/sobel.py
@@ -1,43 +1,34 @@
 import tensorflow as tf
+import numpy as np
 import time
+from timeit import timeit
 
-# Generate a random 512x512 matrix
-input_data = tf.random.normal([2048,2048])
+# Define the matrix dimensions
+image_size = 512
+#image_size = 2048
+#image_size = 8192
 
-# Create the Sobel filter
-sobel_filter = tf.constant([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=tf.float32)
-sobel_filter = tf.reshape(sobel_filter, [3, 3, 1, 1])
+# Create a random image
+image = tf.random.normal(shape=(1, image_size, image_size, 3), dtype=tf.float32)
 
+# Define the Sobel edge detection function
+@tf.function
+def sobel_edge_detection():
+    # Apply the Sobel edge detection using tf.image.sobel_edges
+    gradients = tf.image.sobel_edges(image)
+    return gradients
 
-# Create a function for applying the filter to the input matrix
-def apply_sobel_filter(input_data):
-    # Reshape the input matrix into a 4D tensor (batch_size=1, height=512, width=512, channels=1)
-    input_data = tf.reshape(input_data, [1, 2048,2048, 1])
+# Run the Sobel edge detection on the CPU and measure the time taken
+with tf.device('/device:CPU:0'):
+    cpu_time = timeit(sobel_edge_detection)
+    print("CPU time:", cpu_time)
 
-    # Apply the Sobel filter to the input matrix using conv2d
-    output_data = tf.nn.conv2d(input_data, sobel_filter, strides=[1, 1, 1, 1], padding='SAME')
+# Run the Sobel edge detection on the GPU and measure the time taken
+with tf.device('/device:GPU:0'):
+    gpu_time = timeit(sobel_edge_detection)
+    print("GPU time:", gpu_time)
 
-    # Reshape the output matrix back to a 2D matrix (height=512, width=512)
-    output_data = tf.reshape(output_data,2048,2048])
+# Compute the speedup achieved by running the Sobel edge detection on the GPU
+speedup = cpu_time / gpu_time
+print("Speedup:", speedup)
 
-    return output_data
-
-# Test the function on CPU
-with tf.device('/cpu:0'):
-    start_time_cpu = time.time()
-    output_cpu = apply_sobel_filter(input_data)
-    end_time_cpu = time.time()
-
-# Test the function on GPU
-with tf.device('/gpu:0'):
-    start_time_gpu = time.time()
-    output_gpu = apply_sobel_filter(input_data)
-    end_time_gpu = time.time()
-
-# Compute the speedup time
-speedup_time = (end_time_cpu - start_time_cpu) / (end_time_gpu - start_time_gpu)
-
-# Print the results
-print('Time taken on CPU:', end_time_cpu - start_time_cpu)
-print('Time taken on GPU:', end_time_gpu - start_time_gpu)
-print('Speedup time:', speedup_time)
