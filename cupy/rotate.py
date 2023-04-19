d00003e590e77d8aa5c860ce14f072d5479fbb51
Alex Groeger <groeger.2@wright.edu>
2023-04-19 14:36:31 -0500

    fixed a reference issue

--- cupy/rotate.py
+++ cupy/rotate.py
@@ -12,12 +12,13 @@ angle=45
 
 def dataFunction(size):
 
-    a = np.random.uniform(0,1,size=(size,size,3))
+    a = np.random.randint(0,255,size=(size,size,3),dtype=np.uint8)
     ga = cp.array(a)
     
     return a, ga
 
 def cpuFunction(data):
+    size = data.shape[0]
     return cv2.warpAffine(data,cv2.getRotationMatrix2D((size//2,size//2),angle,1),dsize=(size,size))
 
 def gpuFunction(data):
