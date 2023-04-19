72f646aa63bfe8021693c43824de338fa328a0b1
Alex Groeger <groeger.2@wright.edu>
2023-04-19 14:58:50 -0500

    added number of runs arg

--- cupy/runit.py
+++ cupy/runit.py
@@ -2,7 +2,7 @@ from timeit import timeit
 
 sizes = (512, 2048, 8192)
 
-def runit(dataFunction, cpuFunction, gpuFunction, gpuMemFunction):
+def runit(dataFunction, cpuFunction, gpuFunction, gpuMemFunction, runs=100):
 
     for size in sizes:
         
@@ -10,13 +10,13 @@ def runit(dataFunction, cpuFunction, gpuFunction, gpuMemFunction):
 
         cpuData, gpuData = dataFunction(size)
         
-        cpuTime = timeit(lambda: cpuFunction(cpuData))
+        cpuTime = timeit(lambda: cpuFunction(cpuData), runs=runs)
         print(f"cpu: {cpuTime:.3e}")
     
-        gpuTime = timeit(lambda: gpuFunction(gpuData), gsync=True)
+        gpuTime = timeit(lambda: gpuFunction(gpuData), gsync=True, runs=runs)
         print(f"gpu: {gpuTime:.3e}")
         print(f"operation speedup: {cpuTime/gpuTime:.2f}")
         
-        gpuMemTime = timeit(lambda: gpuMemFunction(cpuData), gsync=True)
+        gpuMemTime = timeit(lambda: gpuMemFunction(cpuData), gsync=True, runs=runs)
         print(f"gpu+mem: {gpuMemTime:.3e}")
         print(f"total speedup: {cpuTime/gpuMemTime:.2f}")
