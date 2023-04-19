461295b99182775bf3ccb68bdd642375cb5ebe90
Lakshmi Bommareddy <w306lxb@login01.cluster>
2023-04-18 21:08:41 -0500

    update sobel edge detection

--- tensorflow/resize.py
+++ tensorflow/resize.py
@@ -1,84 +1,57 @@
-import time
-import numpy as np
 import tensorflow as tf
+import numpy as np
+import time
+from timeit import timeit
 
-# Set random seed for reproducibility
-tf.random.set_seed(42)
+# Define the matrix dimensions
+image_size = 8192
 
-# Generate a random image
-image_size = np.random.randint(512, 512)  # Random image size between 1024 and 4096
-image = np.random.rand(image_size, image_size, 3).astype(np.float32)
+# Create a random image
+image = tf.random.normal(shape=(1, image_size, image_size, 3), dtype=tf.float32)
 
-# Define the new image size for resizing
-new_image_size = np.random.randint(512, 512)  # Random new image size between 1024 and 4096
-
-# Define the bicubic interpolation function
-def bicubic_interpolation(image, new_size):
-    # Expand the dimensions of the input image
-    image = tf.expand_dims(image, axis=0)
-                    
-    # Perform bicubic interpolation
-    resized_image = tf.image.resize(image, [new_size, new_size], method='bicubic')
-                            
-    # Remove the first dimension of the output tensor
-    resized_image = tf.squeeze(resized_image, axis=0)
-                                    
+# Define the TensorFlow function for bicubic interpolation
+@tf.function
+def resize_bicubic():
+    # Apply the bicubic interpolation using tf.image.resize with cubic interpolation method
+    resized_image = tf.image.resize(image, size=(256, 256), method=tf.image.ResizeMethod.BICUBIC)
     return resized_image
 
-# Define the bilinear interpolation function
-def bilinear_interpolation(image, new_size):
-    # Expand the dimensions of the input image
-    image = tf.expand_dims(image, axis=0)
-                                                
-    # Perform bilinear interpolation
-    resized_image = tf.image.resize(image, [new_size, new_size], method='bilinear')
-                                                        
-    # Remove the first dimension of the output tensor
-    resized_image = tf.squeeze(resized_image, axis=0)
-                                                                
+# Define the TensorFlow function for bilinear interpolation
+@tf.function
+def resize_bilinear():
+    # Apply the bilinear interpolation using tf.image.resize with bilinear interpolation method
+    resized_image = tf.image.resize(image, size=(256, 256), method=tf.image.ResizeMethod.BILINEAR)
     return resized_image
 
-# Perform bicubic interpolation on the CPU
-start_time_cpu = time.time()
-resized_image_cpu_bicubic = bicubic_interpolation(image, new_image_size)
-end_time_cpu = time.time()
+# Run the bicubic interpolation on the CPU and measure the time taken
+with tf.device('/device:CPU:0'):
+    cpu_time_bicubic = timeit(resize_bicubic)
+    print("CPU time of Bicubic:", cpu_time_bicubic)
 
-# Calculate the CPU execution time for bicubic interpolation
-cpu_time_bicubic = end_time_cpu - start_time_cpu
-print(f"CPU execution time for bicubic interpolation: {cpu_time_bicubic:.6f} seconds")
+# Run the bicubic interpolation on the GPU and measure the time taken
+with tf.device('/device:GPU:0'):
+    gpu_time_bicubic = timeit(resize_bicubic)
+    print("GPU time of Bicubic:", gpu_time_bicubic)
 
-# Perform bicubic interpolation on the GPU
-with tf.device('/GPU:0'):
-    start_time_gpu = time.time()
-    resized_image_gpu_bicubic = bicubic_interpolation(image, new_image_size)
-    end_time_gpu = time.time()
-# Calculate the GPU execution time for bicubic interpolation
-gpu_time_bicubic = end_time_gpu - start_time_gpu
-print(f"GPU execution time for bicubic interpolation: {gpu_time_bicubic:.6f} seconds")
-
-# Calculate the speedup for bicubic interpolation
+# Compute the speedup achieved by running the bicubic interpolation on the GPU
 speedup_bicubic = cpu_time_bicubic / gpu_time_bicubic
-print(f"Speedup for bicubic interpolation: {speedup_bicubic:.6f}")
+print("Speedup time of Bicubic:", speedup_bicubic)
 
-# Perform bilinear interpolation on the CPU
-start_time_cpu = time.time()
-resized_image_cpu_bilinear = bilinear_interpolation(image, new_image_size)
-end_time_cpu = time.time()
 
-# Calculate the CPU execution time for bilinear interpolation
-cpu_time_bilinear = end_time_cpu - start_time_cpu
-print(f"CPU execution time for bilinear interpolation: {cpu_time_bilinear:.6f} seconds")
+# Run the bilinear interpolation on the CPU and measure the time taken
+with tf.device('/device:CPU:0'):
+    cpu_time_bilinear = timeit(resize_bilinear)
+    print("CPU time of Bilinear:", cpu_time_bilinear)
 
-# Perform bilinear interpolation on the GPU
-with tf.device('/GPU:0'):
-    start_time_gpu = time.time()
-    resized_image_gpu_bilinear = bilinear_interpolation(image, new_image_size)
-    end_time_gpu = time.time()
 
-# Calculate the GPU execution time for bilinear interpolation
-gpu_time_bilinear = end_time_gpu - start_time_gpu
-print(f"GPU execution time for bilinear interpolation: {gpu_time_bilinear:.6f} seconds")
+# Run the bilinear interpolation on the GPU and measure the time taken
+with tf.device('/device:GPU:0'):
+    gpu_time_bilinear = timeit(resize_bilinear)
+    print("GPU time of Bilinear:", gpu_time_bilinear)
 
-# Calculate the speedup for bilinear interpolation
+
+
+# Compute the speedup achieved by running the bilinear interpolation on the GPU
 speedup_bilinear = cpu_time_bilinear / gpu_time_bilinear
-print(f"Speedup for bilinear interpolation: {speedup_bilinear:.6f}")
+print("Speedup of Bilinear:", speedup_bilinear)
+
