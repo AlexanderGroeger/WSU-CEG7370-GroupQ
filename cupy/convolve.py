25c3de5ef415ec9c156c40ffbe865ada590934ed
Alex Groeger <groeger.2@wright.edu>
2023-04-19 14:12:11 -0500

    added runit to convolve

--- cupy/convolve.py
+++ cupy/convolve.py
@@ -6,39 +6,28 @@ import cupy as cp
 from cupyx.scipy.signal import convolve2d
 import cv2
 from timeit import timeit
+from runit import runit
 
-n=1024
-print(f"Size: {n}x{n}")
+def dataFunction(size):
 
-a = np.random.uniform(0,1,size=(n,n))
-k = np.random.uniform(0,1,size=(5,5))
-ga = cp.array(a)
-gk = cp.array(k)
+    a = np.random.uniform(0,1,size=(size,size))
+    k = np.random.uniform(0,1,size=(5,5))
 
-ta = timeit("small_opencv",lambda: cv2.filter2D(a, ddepth=-1, kernel=k),timeout=20)
-print(f"cpu time: {ta:.4e}")
-tb = timeit("small_cupy",lambda: convolve2d(ga,gk),timeout=10,gsync=True)
-print(f"gpu time: {tb:.4e}")
-print(f"operation speedup: {ta/tb:.2f}")
-tc = timeit("small_cupy_memory",lambda: convolve2d(cp.array(a),cp.array(k)).get(),timeout=10,gsync=True)
-print(f"gpu+mem time: {tc:.4e}")
-print(f"total speedup: {ta/tc:.2f}")
+    ga = cp.array(a)
+    gk = cp.array(k)
 
-del ga
-del a
+    return (a,c), (ga,gc)
 
+def cpuFunction(data):
+    a, k = data
+    return cv2.filter2D(a, ddepth=-1, kernel=k)
 
-N=8192
-print(f"Size: {N}x{N}")
+def gpuFunction(data):
+    ga, gc = data
+    return convolve2d(ga,gk)
 
-A = np.random.uniform(0,1,size=(N,N))
-gA = cp.array(A)
+def gpuMemFunction(data):
+    a, c = data
+    return convolve2d(cp.array(a),cp.array(k)).get()
 
-tA = timeit("big_opencv",lambda: cv2.filter2D(A, ddepth=-1, kernel=k),timeout=60)
-print(f"cpu time: {tA:.4e}")
-tB = timeit("big_cupy",lambda: convolve2d(gA,gk),timeout=30,gsync=True)
-print(f"gpu time: {tB:.4e}")
-print(f"operation speedup: {tA/tB:.2f}")
-tC = timeit("big_cupy_memory",lambda: convolve2d(cp.array(A),cp.array(k)).get(),timeout=30,gsync=True)
-print(f"gpu+mem time: {tC:.4e}")
-print(f"total speedup: {tA/tC:.2f}")
+runit(dataFunction, cpuFunction, gpuFunction, gpuMemFunction)
