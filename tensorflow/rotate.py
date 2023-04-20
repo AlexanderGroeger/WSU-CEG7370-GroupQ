d028a81e4508ff5e10da0cf269b68227c98208d3
HarshiniDP <128522200+HarshiniDP@users.noreply.github.com>
2023-04-20 14:42:14 -0400

    Update rotate.py

--- tensorflow/rotate.py
+++ tensorflow/rotate.py
@@ -1,42 +1,55 @@
 import tensorflow as tf
 import numpy as np
 import time
+from timeit import timeit
+from PIL import Image
+import sys
 
-# Generate random input data
-#input_data = np.random.rand(1, 512, 512).astype(np.float32)
-#input_data = np.random.rand(1, 4096, 4096).astype(np.float32)
-input_data = np.random.rand(1, 8192, 8192).astype(np.float32)
+
+if len(sys.argv) > 1:
+     n = sys.argv[1]
+     #Generate random input data
+     input_data = np.random.rand(1, int(n), int(n)).astype(np.float32)
+     print("Size = ", n, "x", n)
+else:
+    input_image = Image.open("s-l1600.jpg") # Load input image
+    input_data = np.array(input_image).astype(np.float32) # Convert input image to numpy array
+    input_data = np.expand_dims(input_data, axis=0) # Add batch dimension
 
 # Define the rotation function
 def rotate_matrix(input_matrix):
-    angle = np.radians(45)
-    rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
-    rotated_matrix = tf.nn.conv2d(np.expand_dims(input_matrix, axis=-1), tf.constant(rotation_matrix, dtype=tf.float32, shape=(2, 2, 1, 1)), strides=[1, 1, 1, 1], padding='SAME')
-    return rotated_matrix
+     angle = np.radians(90)
+     #rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+     #rotated_matrix = tf.nn.conv2d(np.expand_dims(input_matrix, axis=-1), tf.constant(rotation_matrix, dtype=tf.float32, shape=(2, 2, 1, 1)), strides=[1, 1, 1, 1], padding='SAME')
+     rotated_matrix = tf.image.rot90(input_matrix, k=int(angle // (np.pi / 2)))
+     return rotated_matrix
 
-# Define the model
-def model(input_data):
-    rotated_output = rotate_matrix(input_data)
-    return rotated_output
 
 # Define the CPU and GPU sessions
-with tf.device('/cpu:0'): # set device to CPU
-    input_tensor = tf.constant(input_data)
-    output_tensor = model(input_tensor)
-    start_time_cpu = time.time()
-    rotated_data_cpu = output_tensor.numpy()
-    end_time_cpu = time.time()
+def run_on_cpu():
+    with tf.device('/cpu:0'): # set device to CPU
+        input_tensor = tf.constant(input_data)
+        output_tensor = rotate_matrix(input_tensor)
+        rotated_data_cpu = output_tensor.numpy()
+        #rotated_image_cpu = Image.fromarray(rotated_data_cpu[0, 0, :, :, 0].astype(np.uint8)) # Convert rotated data to image
+        if(len(sys.argv) < 2):
+            rotated_image_cpu = Image.fromarray(rotated_data_cpu[0].astype(np.uint8)) # Convert rotated data to image
+            rotated_image_cpu.save("output_image_cpu.jpg") # Save output image from CPU
 
-with tf.device('/device:GPU:0'): # set device  to GPU
-    input_tensor = tf.constant(input_data)
-    output_tensor = model(input_tensor)
-    start_time_gpu = time.time()
-    rotated_data_gpu = output_tensor.numpy()
-    end_time_gpu = time.time()
+def run_on_gpu():
+    with tf.device('/device:GPU:0'): # set device to GPU
+        input_tensor = tf.constant(input_data)
+        output_tensor = rotate_matrix(input_tensor)
+        rotated_data_gpu = output_tensor.numpy()
+        #rotated_image_gpu = Image.fromarray(rotated_data_gpu[0, 0, :, :, 0].astype(np.uint8)) # Convert rotated data to image
+        if(len(sys.argv) < 2):
+            rotated_image_gpu = Image.fromarray(rotated_data_gpu[0].astype(np.uint8)) # Convert rotated data to image
+            rotated_image_gpu.save("output_image_gpu.jpg") # Save output image from CPU
+
 
 # Calculate and print the speedup
-cpu_time = end_time_cpu - start_time_cpu
-gpu_time = end_time_gpu - start_time_gpu
+cpu_time = timeit(run_on_cpu) # Measure CPU time
+gpu_time = timeit(run_on_gpu) # Measure GPU time
 speedup = cpu_time / gpu_time
 print("CPU time:", cpu_time)
 print("GPU time:", gpu_time)
