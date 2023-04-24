ba8f2ee0b3df566a09509bfd5f3d983fb6dc2c96
HarshiniDP <128522200+HarshiniDP@users.noreply.github.com>
2023-04-24 17:25:36 -0400

    Update MatrixMult.py

--- tensorflow/MatrixMult.py
+++ tensorflow/MatrixMult.py
@@ -1,37 +1,29 @@
 import tensorflow as tf
+import sys
 from timeit import timeit
 
-sizes = [512, 2048, 8192]
+if len(sys.argv) >1 :
+    matrix_size = sys.argv[1]
+    matrix1 = tf.random.normal(shape=(int(matrix_size), int(matrix_size)))
+    matrix2 = tf.random.normal(shape=(int(matrix_size), int(matrix_size)))
+    print("Size: ", matrix_size, 'X', matrix_size)
 
-for size in sizes:
-    # create two large matrices
-    matrix1 = tf.random.normal([size, size])
-    matrix2 = tf.random.normal([size, size])
+else:
+    printf("Please pass valid paraeter with the script")
+    exit(0)
 
-    # define function to perform matrix multiplication on GPU
-    @tf.function
-    def gpu_multiply(a, b):
-        with tf.device('/GPU:0'):
-            return tf.matmul(a, b)
+def matrixMultiplication(matrix1, matrix2):
+    return tf.matmul(matrix1, matrix2)
 
-    # copy matrices to CPU memory
-    cpu_copy_time = timeit(lambda: tf.identity(matrix1))
 
-    # copy matrices to GPU memory
-    gpu_copy_time = timeit(lambda: tf.identity(matrix1).gpu())
+with tf.device('/CPU:0'):
+    cpu_time = timeit(lambda: matrixMultiplication(matrix1, matrix2))
 
-    # perform matrix multiplication on CPU
-    cpu_time = timeit(lambda: tf.matmul(matrix1, matrix2))
 
-    # perform matrix multiplication on GPU
-    gpu_time = timeit(lambda: tf.matmul(matrix1.gpu(), matrix2.gpu()))
+with tf.device('/GPU:0'):
+    gpu_time = timeit(lambda: matrixMultiplication(matrix1, matrix2))
 
-    operation_speedup = cpu_time / gpu_time
 
-    print(f"Matrix size: {size}")
-    print(f'CPU copy time: {cpu_copy_time:.3e}')
-    print(f'GPU copy time: {gpu_copy_time:.3e}')
-    print(f'CPU time: {cpu_time:.3e}')
-    print(f'GPU time: {gpu_time:.3e}')
-    print(f'Operation speedup: {operation_speedup:.2f}')
-    print()
+print("CPU time: ", cpu_time, " seconds")
+print("GPU time: ", gpu_time," seconds")
+speedup = cpu_time / gpu_time
