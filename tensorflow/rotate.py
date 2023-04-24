befc63a1654e9848c4bd8f70baa1d9005421eff7
HarshiniDP <128522200+HarshiniDP@users.noreply.github.com>
2023-04-24 15:31:28 -0400

    Update rotate.py

--- tensorflow/rotate.py
+++ tensorflow/rotate.py
@@ -10,11 +10,6 @@ if len(sys.argv) > 1:
      image = tf.random.uniform(shape=(int(n),int(n)), minval=0, maxval=255, dtype=image_dtype)
      print("Size = ", n, "x", n)
 
-else:
-    image_path = 'image.jpg'
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image)
-
 angle = np.deg2rad(45)
 
 def rotateImage(image, angle):
@@ -22,21 +17,21 @@ def rotateImage(image, angle):
     rotated_image = tf.cast(rotated_image, tf.uint8)
     return rotated_image
 
-def run_on_cpu():
-    with tf.device('/CPU:0'):
-        rotated_image = rotateImage(image, angle)
-        if len(sys.argv) < 2:
-            tf.io.write_file('rotated_cpu_output.jpg', tf.image.encode_jpeg(rotated_image))
-
-def run_on_gpu():
-    with tf.device('/GPU:0'):
-        rotated_image = rotateImage(image, angle)
-        if len(sys.argv) < 2:
-            tf.io.write_file('rotated_gpu_output.jpg', tf.image.encode_jpeg(rotated_image))
+#To Rotate Actual Image
+if (len(sys.argv)<2):
+    image_path = 'image.jpg'
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_jpeg(image)
+    rotated_image = rotateImage(image, angle)
+    tf.io.write_file('rotated_output.jpg', tf.image.encode_jpeg(rotated_image))
+    exit(0)
 
 
-cpu_time = timeit(run_on_cpu)
-gpu_time = timeit(run_on_gpu)
+with tf.device('/CPU:0'):
+    cpu_time = timeit(lambda: rotateImage(image, angle))
+
+with tf.device('/GPU:0'):
+    gpu_time = timeit(lambda: rotateImage(image, angle))
 
 print('CPU Time: ', cpu_time)
 print('GPU Time: ', gpu_time)
