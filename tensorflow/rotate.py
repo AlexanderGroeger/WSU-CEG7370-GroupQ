9ee856ca5b4bc8b446f6a5e4ad611689fbf2f4d6
HarshiniDP <128522200+HarshiniDP@users.noreply.github.com>
2023-04-24 19:14:06 -0400

    Update rotate.py

--- tensorflow/rotate.py
+++ tensorflow/rotate.py
@@ -2,13 +2,6 @@ import tensorflow as tf
 import tensorflow_addons as tfa
 import numpy as np
 from timeit import timeit
-import sys
-
-if len(sys.argv) > 1:
-     n = sys.argv[1]
-     image_dtype = tf.float32
-     image = tf.random.uniform(shape=(int(n),int(n)), minval=0, maxval=255, dtype=image_dtype)
-     print("Size = ", n, "x", n)
 
 angle = np.deg2rad(45)
 
@@ -18,21 +11,28 @@ def rotateImage(image, angle):
     return rotated_image
 
 #To Rotate Actual Image
-if (len(sys.argv)<2):
-    image_path = 'image.jpg'
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image)
-    rotated_image = rotateImage(image, angle)
-    tf.io.write_file('rotated_output.jpg', tf.image.encode_jpeg(rotated_image))
-    exit(0)
+image_path = 'image.jpg'
+image = tf.io.read_file(image_path)
+image = tf.image.decode_jpeg(image)
+rotated_image = rotateImage(image, angle)
+tf.io.write_file('rotated_output.jpg', tf.image.encode_jpeg(rotated_image))
 
+n=512
 
-with tf.device('/CPU:0'):
-    cpu_time = timeit(lambda: rotateImage(image, angle))
+for i in range(1, 4):
 
-with tf.device('/GPU:0'):
-    gpu_time = timeit(lambda: rotateImage(image, angle))
+    image_dtype = tf.float32
+    image = tf.random.uniform(shape=(n, n), minval=0, maxval=255, dtype=image_dtype)
+    print("Size = ", n, "x", n)
 
-print('CPU Time: ', cpu_time)
-print('GPU Time: ', gpu_time)
-print('Speed Up: ', cpu_time/gpu_time)
+    n=n*4
+
+    with tf.device('/CPU:0'):
+        cpu_time = timeit(lambda: rotateImage(image, angle))
+
+    with tf.device('/GPU:0'):
+        gpu_time = timeit(lambda: rotateImage(image, angle))
+
+    print('CPU Time: ', cpu_time)
+    print('GPU Time: ', gpu_time)
+    print('Speed Up: ', cpu_time/gpu_time)
