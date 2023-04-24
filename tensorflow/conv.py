fcb492621761f0b14484b697b95b4937a8733875
HarshiniDP <128522200+HarshiniDP@users.noreply.github.com>
2023-04-24 13:48:08 -0400

    Update conv.py

--- tensorflow/conv.py
+++ tensorflow/conv.py
@@ -11,13 +11,16 @@ input_data = tf.random.normal([1, n, n, 1])
 
 kernel = tf.constant(tf.ones(shape=[1, 5, 5, 1]), dtype=tf.float32)
 
+def convolution(matrix):
+    return tf.nn.conv2d(matrix, kernel, strides=[1, 1, 1, 1], padding="VALID")
+
 # Test CPU execution time
 with tf.device('/CPU:0'):
-    cpu_time = timeit(lambda: tf.nn.conv2d(input_data, kernel, strides=[1, 1, 1, 1], padding="VALID"))
+    cpu_time = timeit(lambda: convolution(input_data))
 
 # Test GPU execution time
 with tf.device('/GPU:0'):
-    gpu_time = timeit(lambda: tf.nn.conv2d(input_data, kernel, strides=[1, 1, 1, 1], padding="VALID"))
+    gpu_time = timeit(lambda: convolution(input_data))
 
 print("CPU Time", cpu_time)
 print("GPU Time", gpu_time)
