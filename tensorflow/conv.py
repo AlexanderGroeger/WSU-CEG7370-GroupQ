b9beabbcf2aef52fdbf0c1c30d0e4c4a0413ee0c
HarshiniDP <128522200+HarshiniDP@users.noreply.github.com>
2023-04-24 19:29:01 -0400

    Update conv.py

--- tensorflow/conv.py
+++ tensorflow/conv.py
@@ -1,27 +1,28 @@
 import tensorflow as tf
 import time
 from timeit import timeit
-import sys
 import numpy as np
 
-# Generate random input data
-n = int(sys.argv[1])
-print("Size: ",n,"X",n)
-input_data = tf.random.normal([1, n, n, 1])
-
-kernel = tf.constant(tf.ones(shape=[1, 5, 5, 1]), dtype=tf.float32)
+kernel = tf.constant(tf.ones(shape=[ 5, 5, 1, 1]), dtype=tf.float32)
 
 def convolution(matrix):
     return tf.nn.conv2d(matrix, kernel, strides=[1, 1, 1, 1], padding="VALID")
 
-# Test CPU execution time
-with tf.device('/CPU:0'):
-    cpu_time = timeit(lambda: convolution(input_data))
+n=512
 
-# Test GPU execution time
-with tf.device('/GPU:0'):
-    gpu_time = timeit(lambda: convolution(input_data))
+for i in range(1, 4):
 
-print("CPU Time", cpu_time)
-print("GPU Time", gpu_time)
-print("Speed Up :", cpu_time/gpu_time)
+    print("Size: ",n,"X",n)
+    input_data = tf.random.normal([1, n, n, 1])
+
+    n=n*4
+
+    with tf.device('/CPU:0'):
+        cpu_time = timeit(lambda: convolution(input_data))
+
+    with tf.device('/GPU:0'):
+        gpu_time = timeit(lambda: convolution(input_data))
+
+    print("CPU Time", cpu_time)
+    print("GPU Time", gpu_time)
+    print("Speed Up :", cpu_time/gpu_time)
