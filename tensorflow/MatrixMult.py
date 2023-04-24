83c1d874ab32a6964b4b2fcea9cf45a4c9b6b797
HarshiniDP <128522200+HarshiniDP@users.noreply.github.com>
2023-04-24 19:06:29 -0400

    Update MatrixMult.py

--- tensorflow/MatrixMult.py
+++ tensorflow/MatrixMult.py
@@ -2,28 +2,28 @@ import tensorflow as tf
 import sys
 from timeit import timeit
 
-if len(sys.argv) >1 :
-    matrix_size = sys.argv[1]
-    matrix1 = tf.random.normal(shape=(int(matrix_size), int(matrix_size)))
-    matrix2 = tf.random.normal(shape=(int(matrix_size), int(matrix_size)))
-    print("Size: ", matrix_size, 'X', matrix_size)
-
-else:
-    printf("Please pass valid paraeter with the script")
-    exit(0)
-
 def matrixMultiplication(matrix1, matrix2):
     return tf.matmul(matrix1, matrix2)
 
+n=512
 
-with tf.device('/CPU:0'):
-    cpu_time = timeit(lambda: matrixMultiplication(matrix1, matrix2))
+for i in range(1,4):
+
+    matrix1 = tf.random.normal(shape=(n,n))
+    matrix2 = tf.random.normal(shape=(n, n))
+    print("Size: ", n, 'X', n)
+
+    n = n*4
+
+    with tf.device('/CPU:0'):
+        cpu_time = timeit(lambda: matrixMultiplication(matrix1, matrix2))
 
 
-with tf.device('/GPU:0'):
-    gpu_time = timeit(lambda: matrixMultiplication(matrix1, matrix2))
+    with tf.device('/GPU:0'):
+        gpu_time = timeit(lambda: matrixMultiplication(matrix1, matrix2))
 
 
-print("CPU time: ", cpu_time, " seconds")
-print("GPU time: ", gpu_time," seconds")
-speedup = cpu_time / gpu_time
+    print("CPU time: ", cpu_time, " seconds")
+    print("GPU time: ", gpu_time," seconds")
+    speedup = cpu_time / gpu_time
+    print("Speedup: ", speedup)
